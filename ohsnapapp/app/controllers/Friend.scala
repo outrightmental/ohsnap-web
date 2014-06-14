14e8227c93ecc739789f395a4305e868b7905d5d
Jesus Luzon <jesus.e.luzon@gmail.com>
2014-06-14 15:08:42 -0400

    Added method to parse Modules to HTML

--- ohsnapapp/app/controllers/Friend.scala
+++ ohsnapapp/app/controllers/Friend.scala
@@ -12,5 +12,17 @@ object Friend extends Controller {
   def view(id:String) = Action {
     Ok(views.html.friend_view("Here is a picure of your friend " + id))
   }
+  def method(arr: Array[Module]) = {
+    val sb = new StringBuilder();
+    arr.foreach( x => sb.append( "<div class=\"col-sm-6 col-md-3\">" +  
+            "<h1>" + x.keyword + "</h1>" +
+            "<img src=\"/assets/images/friends/stable-love.gif\" />" +
+            "<h2>"+x.text+"</h2>" +
+        "</div>"));
+    sb.toString;
+  }
 
+  
 }
+
+case class Module(keyword: String, text: String)
